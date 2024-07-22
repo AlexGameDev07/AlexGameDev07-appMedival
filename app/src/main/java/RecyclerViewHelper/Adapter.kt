65d77b709c5be056af9b3abe51cbabef29261602
AlexDev <alekeymurcia@gmail.com>
2024-07-21 23:46:09 -0600

    agregué todo lo del adapter pero aun le falta

--- app/src/main/java/RecyclerViewHelper/Adapter.kt
+++ app/src/main/java/RecyclerViewHelper/Adapter.kt
@@ -1,22 +1,183 @@
 package RecyclerViewHelper
 
+import Model.Connection
 import Model.DataClassPacientes
+import alejando.murcia.jesus.arce.medival.PacientesActivity
+import alejando.murcia.jesus.arce.medival.R
+import android.app.AlertDialog
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class Adapter(private var Data: List<DataClassPacientes>) : RecyclerView.Adapter<ViewHolder>() {
+class Adapter(private var Data: List<DataClassPacientes>) : RecyclerView.Adapter<RecyclerViewHelper.ViewHolder>() {
 
     fun RecargarVista(newDataList: List<DataClassPacientes>){
         Data = newDataList
         notifyDataSetChanged()
     }
 
-    fun actualizarListaDespuesDeActualizarDatos(uuid: String, nuevoNombre: String){
-        val index = Datos.indexOfFirst { it.uuid == uuid }
-        Datos[index].NombreProducto= nuevoNombre
+    fun actualizarListaDespuesDeActualizarDatos(idPacientes: Int, nuevoNombre: String){
+        val index = Data.indexOfFirst { it.idPacientes == idPacientes }
+        Data[index].nombres= nuevoNombre
         notifyItemChanged(index)
     }
 
-    //Aun esta incompleto esto
+    fun eliminarRegistro(ID_Pacientes:Int,position: Int){
+
+        //quitar el elementpo de la lista
+        val listaDatos = Data.toMutableList()
+        listaDatos.removeAt(position)
+
+        //quitar de la base de datos
+        GlobalScope.launch(Dispatchers.IO) {
+
+            //crear un objeto e la clase conexion
+            val objConexion=Connection().Connect()
+
+            val deletePaciente = objConexion?.prepareStatement("DELETE TB_Pacientes WHERE idPaciente = ?")!!
+            deletePaciente.setInt( 1,ID_Pacientes)
+            deletePaciente.executeUpdate()
+
+            val commit = objConexion.prepareStatement( "commit")!!
+            commit.executeUpdate()
+        }
+        Data=listaDatos.toList()
+        notifyItemRemoved(position)
+        notifyDataSetChanged()
+
+    }
+
+    fun actualizarProducto(Nombres: String, Apellidos: String, Edad: Int, NumCama: Int, ID_Paciente: Int){
+
+        //1- CREO UNA CORRUTINA
+        GlobalScope.launch(Dispatchers.IO){
+
+            //1- Creo un objeto de tipo conexion
+            val objConexion = Connection().Connect()
+
+            //2- Creo una variable un prepareStatement
+            val updateProducto = objConexion?.prepareStatement("update TB_Pacientes set Nombres = ? Apellidos = ? Edad = ? Num_Cama = ? where uuid =?")!!
+            updateProducto.setString(1, Nombres)
+            updateProducto.setString(2, Apellidos)
+            updateProducto.setInt(3, Edad)
+            updateProducto.setInt(4, NumCama)
+            updateProducto.setInt(5, ID_Paciente)
+            updateProducto.executeUpdate()
+
+            val commit = objConexion.prepareStatement("commit")!!
+            commit.executeUpdate()
+
+            withContext(Dispatchers.Main){
+                actualizarListaDespuesDeActualizarDatos(ID_Paciente, Nombres)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHelper.ViewHolder {
+        val vista = LayoutInflater.from(parent.context).inflate(R.layout.activity_item_card, parent, false)
+        return RecyclerViewHelper.ViewHolder(vista)
+    }
+
+    override fun getItemCount() = Data.size
+
+    override fun onBindViewHolder(holder: RecyclerViewHelper.ViewHolder, position: Int) {
+        val pacientes = Data[position]
+        holder.textView.text = pacientes.nombres
+
+        val item =Data[position]
+
+
+        holder.imgBorrar.setOnClickListener {
+            //craeamos una alaerta
+
+            //invocamos  el contexto
+            val context = holder.itemView.context
+
+            //CREO LA ALERTA
+
+            val builder = AlertDialog.Builder(context)
+
+            //le ponemos titulo a la alerta
+
+            builder.setTitle("¿estas seguro?")
+
+            //ponerle mendsaje a la alerta
+
+            builder.setMessage("Deseas en verdad eliminar el registro")
+
+            //agrgamos los botones
+
+            builder.setPositiveButton("si"){dialog,wich ->
+                eliminarRegistro(item.idPacientes,position)
+            }
+
+            builder.setNegativeButton("no"){dialog,wich ->
+
+            }
+
+            //cramos la alerta
+            val alertDialog=builder.create()
+
+            //mostramos la alerta
+
+            alertDialog.show()
+
+        }
+
+        holder.imgEditar.setOnClickListener {
+
+            val context = holder.itemView.context
+
+            //Creo la alerta
+            val  builder = AlertDialog.Builder(context)
+            builder.setTitle("Editar nombre")
+
+            //Agreguemos un cuadro de textos para que el usuario pueda escribir el nuevo nombre
+            val cuadritoNuevoNombre = EditText(context)
+            cuadritoNuevoNombre.setHint(item.nombres)
+            builder.setView(cuadritoNuevoNombre)
+
+            builder.setPositiveButton("Actualizar "){ dialog, wich ->
+                actualizarProducto(cuadritoNuevoNombre.text.toString(), item.apellidos, item.idPacientes, item.edad, item.numCama)
+
+            }
+
+            builder.setNegativeButton( "Cancelar"){ dialog, wich ->
+                dialog.dismiss()
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+        }
+
+        //darle click a la card
+        holder.itemView.setOnClickListener{
+            val context = holder.itemView.context
+
+            //Cambiamos de pantalla
+            //Abro la pantalla de productos
+            val pantallaDetalles = Intent(context, PacientesActivity::class.java)
+
+            //Abriremos la pantalla
+            //Pero antes mandamos los parametros
+
+            pantallaDetalles.putExtra("ID_Paciente", item.idPacientes )
+            pantallaDetalles.putExtra("Nombres", item.nombres)
+            pantallaDetalles.putExtra("Apellidos", item.apellidos)
+            pantallaDetalles.putExtra("Edad", item.edad)
+            pantallaDetalles.putExtra("NumCama", item.numCama)
+
+            //Inicializamos la actividad
+
+            context.startActivity(pantallaDetalles)
+        }
+
+    }
 
 }
